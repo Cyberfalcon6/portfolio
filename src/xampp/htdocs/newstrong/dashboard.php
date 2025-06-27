06f3f5b48db887057f62981da7c8d5c6635bb742
Japhet Niyogakiza <cyberfalcon660066@gmail.com>
2025-06-27 22:44:46 +0200

    deleting xampp

--- src/xampp/htdocs/newstrong/dashboard.php
+++
@@ -1,4 +0,0 @@
-<a href="dashboard.php" class="navbar-brand">
-    <img src="../stafs/logo.png" alt="Company Logo" class="navbar-logo">
-    Admin Panel
-</a>
--- src/xampp/htdocs/newstrong/member/auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-$login_path = "/newstrong/member/login.php";
-if (!isset($_SESSION['id'])) {
-    header("Location: $login_path");
-    exit();
-}
-?>
