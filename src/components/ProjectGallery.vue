cfe5dbfb67adcd3229cb14f0ac09cc5ba1aef650
Japhet Niyogakiza <cyberfalcon660066@gmail.com>
2025-06-28 21:04:21 +0200

    adding a gallery

--- src/components/ProjectGallery.vue
+++ src/components/ProjectGallery.vue
@@ -4,15 +4,51 @@
             <div class="left-carret"></div>
             <img src="" alt="">
             <div class="right-carret"></div>
+            <div class="details"></div>
         </div>
-        <div class="detail-space">
-            <div class="step"></div>
+        <div class="other">
+           <div class="project"></div>
         </div>
     </div>
 </template>
 <script>
+    export default({
+        data(){
+            return{
+                currentProject: {
+                    name: "", 
+                    photos: [
+                        "@/assets/portfolio.png",
+                        "@/assets/chatapp.jpg",
+                        "@/assets/ecommerce.webp",
+                    ]
+                },
+                projects: [
+                    {
+                        name: "Portfolio Website",
+                        photo: "@/assets/portfolio.jpg"
+                    },
+                    {
+                        name: "Portfolio Website",
+                        photo: "@/assets/portfolio.jpg"
+                    },
+                    {
+                        name: "Portfolio Website",
+                        photo: "@/assets/portfolio.jpg"
+                    },
 
+                ]
+            }
+        }
+    })
 </script>
 <style scoped>
 
+
+.container{
+    width: 100%;
+    height: 100%;
+    background-color: azure;
+
+}
 </style>
